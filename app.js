21ec147aa8f2a06049155a8af0ceffab89deebf4
Fellipe Salles <felli@Fsalles20>
2025-08-21 21:43:28 -0300

    Criação e estilização das sessões sobre mim e formação

--- app.js
+++ app.js
@@ -1,22 +1,41 @@
+//mostra a lista oculta clicando no menu hamburguer
 const menu = document.querySelector("#menu")
 
 const listaMenu = document.querySelector(".lista-links")
 
+const imagemMenu = document.querySelector('.img-menu')
+
+
 menu.addEventListener('change', () => {
     if(menu.checked){
         listaMenu.classList.add("show-list")
+        imagemMenu.src = "assets/close.svg"
         return
     }
 
     listaMenu.classList.remove("show-list")
+    imagemMenu.src = "assets/menu.svg"
 })
 
 
+//fecha o menu hamburguer ao clicar em um link
+const linksLista = document.querySelectorAll(".lista-links a")
+
+linksLista.forEach(link => {
+    link.addEventListener('click', () => {
+        menu.checked = false
+        listaMenu.classList.remove('show-list')
+        imagemMenu.src = "assets/menu.svg"
+    })
+})
+
+
+
+
+//altera a imagem do link do github
 const logoGithub = document.querySelector("#git-red")
 
 const linkGithub = document.querySelector(".link-github")
-
-console.log(logoGithub)
 
 linkGithub.addEventListener('mouseover', () => {
     logoGithub.src = "assets/icones_tecnologias/github_branco.svg"
